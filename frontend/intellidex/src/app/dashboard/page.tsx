71fb38f43759d5652fa75737cb95ccba8475e0ee
Hitesh Lalwani <lwni@protonmail.com>
2024-04-25 15:54:19 +0530

    sidebar wip

---
+++ frontend/intellidex/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+export default function Dashboard() {
+  return (
+    <div className="bg-[#F6E4A1] w-screen h-screen">
+      <div className="grid grid-cols-12 grid-rows-12 w-full h-full">
+        <div className="col-start-1 col-end-2 row-start-1 row-end-13 flex px-8 py-4">
+          <div className="w-full h-full bg-black"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
