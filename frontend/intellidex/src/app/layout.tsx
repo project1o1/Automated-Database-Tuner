0df687ecfc85ebf65d7c8c1c943d79c2ba44cee8
Hitesh Lalwani <lwni@protonmail.com>
2024-04-25 08:23:06 +0530

    lp WIP

--- frontend/intellidex/src/app/layout.tsx
+++ frontend/intellidex/src/app/layout.tsx
@@ -28,6 +28,10 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body className={inter.className + " text-white overflow-hidden"}>
         <div>{children}</div>
