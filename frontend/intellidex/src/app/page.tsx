242d508a229c55cbcef10f2503013ce3e7452e30
Hitesh Lalwani <lwni@protonmail.com>
2024-04-25 08:23:06 +0530

    lp WIP

--- frontend/intellidex/src/app/page.tsx
+++ frontend/intellidex/src/app/page.tsx
@@ -1,22 +1,23 @@
 import Image from "next/image";
 import { Fragment } from "react";
 
-function BackgroundLayer() {
+export default function Home() {
   return (
-    <div className="grid grid-cols-12 grid-rows-12 w-screen h-screen bg-[radial-gradient(circle_at_top_left,rgba(155,214,228,1)_0%,rgba(218,248,255,1)_100%)]">
+    <div className="grid grid-cols-12 grid-rows-12 w-screen h-screen bg-[radial-gradient(ellipse_at_top_left,rgba(155,214,228,1)_60%,rgba(218,248,255,1)_100%)]">
       <div className="col-start-7 col-end-13 row-start-3 row-end-13">
         <img src="/rocket.png" width={800}></img>
         {/* <video src="/db_merged.mp4" width={1200} autoPlay muted loop></video> */}
       </div>
-      <div className="col-start-2 row-start-3 col-end-5 row-end 10">
-        <p className="font-['Bebas_Neue'] text-black text-9xl">
+      <div className="col-start-2 row-start-3 col-end-5 row-end-10">
+        <p className="font-['Bebas_Neue'] text-black text-9xl mb-2">
           make your database wickedly fast
         </p>
+        <button className="bg-[linear-gradient(90deg,hsla(58,100%,68%,1)_0%,_hsla(45,80%,81%,1)100%)] rounded-md">
+          <p className="font-['Roboto'] text-xl mx-12 my-4 font-medium text-black">
+            Get Started
+          </p>
+        </button>
       </div>
     </div>
   );
-}
-
-export default function Home() {
-  return <BackgroundLayer />;
 }
