eca7e26d0d5e091a18488858df05b7aa44c52fc6
Hitesh Lalwani <lwni@protonmail.com>
2024-04-25 07:54:22 +0530

    lp WIP

--- frontend/intellidex/src/app/page.tsx
+++ frontend/intellidex/src/app/page.tsx
@@ -4,8 +4,9 @@ import { Fragment } from "react";
 function BackgroundLayer() {
   return (
     <div className="grid grid-cols-12 grid-rows-12 w-screen h-screen bg-[radial-gradient(circle_at_top_left,rgba(155,214,228,1)_0%,rgba(218,248,255,1)_100%)]">
-      <div className="col-start-6 col-end-13 row-start-5 row-end-13">
-        <video src="/db_merged.mp4" width={1200} autoPlay muted loop></video>
+      <div className="col-start-7 col-end-13 row-start-3 row-end-13">
+        <img src="/rocket.png" width={800}></img>
+        {/* <video src="/db_merged.mp4" width={1200} autoPlay muted loop></video> */}
       </div>
       <div className="col-start-2 row-start-3 col-end-5 row-end 10">
         <p className="font-['Bebas_Neue'] text-black text-9xl">
