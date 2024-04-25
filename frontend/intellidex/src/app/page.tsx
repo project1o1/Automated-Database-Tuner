1c7ae4416b41dee848117879f326556f32da7e54
Hitesh Lalwani <lwni@protonmail.com>
2024-04-25 12:47:36 +0530

    added transition

--- frontend/intellidex/src/app/page.tsx
+++ frontend/intellidex/src/app/page.tsx
@@ -6,14 +6,13 @@ export default function Home() {
     <div className="grid grid-cols-12 grid-rows-12 w-screen h-screen bg-[radial-gradient(ellipse_at_top_left,rgba(11,149,181,1)_60%,rgba(218,248,255,1)_100%)]">
       <div className="col-start-7 col-end-13 row-start-3 row-end-13">
         <img src="/rocket.png" width={800}></img>
-        {/* <video src="/db_merged.mp4" width={1200} autoPlay muted loop></video> */}
       </div>
       <div className="col-start-2 row-start-3 col-end-5 row-end-10">
         <p className="font-['Bebas_Neue'] text-black text-9xl mb-2">
           make your database wickedly fast
         </p>
-        <button className="bg-[linear-gradient(90deg,hsla(58,100%,68%,1)_0%,_hsla(45,80%,81%,1)100%)] rounded-lg">
-          <p className="font-['Roboto'] text-xl mx-12 my-4 font-medium text-black">
+        <button className="bg-[linear-gradient(90deg,hsla(58,100%,68%,1)_0%,_hsla(45,80%,81%,1)100%)] rounded-lg  hover:translate-x-1 hover:-translate-y-1 hover:shadow transition">
+          <p className="font-['Roboto'] text-xl mx-12 my-4 font-medium text-black ">
             Get Started
           </p>
         </button>
@@ -23,13 +22,22 @@ export default function Home() {
           <img src="/ln.png" className="w-full h-full "></img>
         </div>
 
-        <div className="flex text-xl">
-          <div className="mx-5">About us</div>
-          <div className="mx-5">How it works</div>
-          <div className="mx-5">Get In Touch</div>
+        <div className="flex text-xl font-medium">
+          <div className="mx-5 group">
+            <p>About us</p>
+            <div className="h-1 bg-white w-0 group-hover:w-full transition-all"></div>
+          </div>
+          <div className="mx-5 group">
+            <p>How it works</p>
+            <div className="h-1 bg-white w-0 group-hover:w-full transition-all"></div>
+          </div>
+          <div className="mx-5 group">
+            <p>Get In Touch</p>
+            <div className="h-1 bg-white w-0 group-hover:w-full transition-all"></div>
+          </div>
         </div>
 
-        <button className="rounded-lg bg-transparent border-4">
+        <button className="rounded-lg bg-transparent border-4 ">
           <p className=" text-xl font-semibold mx-8 my-4 ">Log In</p>
         </button>
       </div>
