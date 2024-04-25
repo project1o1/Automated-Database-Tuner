dd8a466f7aae7c53affe5617d9c30886e30388cc
Hitesh Lalwani <lwni@protonmail.com>
2024-04-25 07:22:43 +0530

    landing page WIP

--- frontend/intellidex/src/app/page.tsx
+++ frontend/intellidex/src/app/page.tsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import { Fragment } from "react";
 
-export default function Home() {
+function BackgroundLayer() {
   return (
-    <Fragment>
-      <div>hi</div>
-    </Fragment>
+    <div className="grid grid-cols-12 grid-rows-12 w-screen h-screen bg-[radial-gradient(circle_at_top_left,rgba(155,214,228,1)_0%,rgba(218,248,255,1)_100%)]">
+      <div className="col-start-6 col-end-13 row-start-5 row-end-13">
+        <video src="/db_merged.mp4" width={1200} autoPlay muted loop></video>
+      </div>
+      <div className="col-start-2 row-start-3 col-end-5 row-end 10">
+        <p className="font-['Bebas_Neue'] text-black text-9xl">
+          make your database wickedly fast
+        </p>
+      </div>
+    </div>
   );
+}
+
+export default function Home() {
+  return <BackgroundLayer />;
 }
