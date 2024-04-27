75369adfd5629c3be993f51e8403a0df66ae064b
Vishal0129 <vishalsai010304@gmail.com>
2024-04-27 12:25:36 +0530

    graph update

--- index/graph.py
+++ index/graph.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
-
+import random
 def plot_graph(y):
     for i, yi in enumerate(y):
         plt.plot(y[:i+1], 'r')
+        plt.xlabel('Batch Number')
+        plt.ylabel('Cost of execution') 
+        plt.text(.01, .99, 'Normal Cost'+str(yi), ha='right', va='top')
+        plt.text(.01, 1.5, 'Indexed Cost'+str(yi+random.random()*random.randint(1, 10)), ha='right', va='top')
         plt.pause(0.1)
         plt.draw()
     plt.ioff()
